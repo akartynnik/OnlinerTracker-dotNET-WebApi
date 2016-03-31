2bc6b3b47a22971128c481a31794e9d7ff2b87eb
Alexey Kartynnik <alex.kar.008@gmail.com>
2016-03-31 17:28:24 +0300

    fixed bug: when prise of product was NULL we get angular ERROR and UI slower

--- OnlinerTracker.Web/app/filters.js
+++ OnlinerTracker.Web/app/filters.js
@@ -1,6 +1,8 @@
 ﻿'use strict';
 angular.module('filters', []).filter('moneySeparatorAdd', function () {
-    return function (input) {
-        return input.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+    return function (prices) {
+        if (!prices)
+            return "Price wasn't set...";
+        return prices.min.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ') + ' BLR';
     };
 });
