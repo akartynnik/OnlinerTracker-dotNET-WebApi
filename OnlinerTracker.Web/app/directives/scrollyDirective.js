cd13292395c52476459ed885bd250d77dc4bf4ff
Alexey Kartynnik <alex.kar.008@gmail.com>
2016-04-01 16:43:58 +0300

    add custom directive for infinit scrolling

---
+++ OnlinerTracker.Web/app/directives/scrollyDirective.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+app.directive('scrolly', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var raw = element[0];
+            console.log('loading directive');
+
+            element.bind('scroll', function () {
+                if (raw.scrollTop + raw.offsetHeight === raw.scrollHeight) {
+                    scope.$apply(attrs.scrolly);
+                }
+            });
+        }
+    };
+});
