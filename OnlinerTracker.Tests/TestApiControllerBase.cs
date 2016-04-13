2a77c7b290915c0b3d409105f7d5a3bea50adabe
Alexey Kartynnik <alex.kar.008@gmail.com>
2016-04-13 18:51:22 +0300

    add tests for several methods of ApiControllerBase

---
+++ OnlinerTracker.Tests/TestApiControllerBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlinerTracker.Api.Controllers;
+using System.Web.Http.Results;
+
+namespace OnlinerTracker.Tests
+{
+    [TestClass]
+    public class TestApiControllerBase
+    {
+        [TestMethod]
+        public void Successful_ShouldReturnOkNegotiatedContentResultWithOkMessage()
+        {
+            var controller = new ApiControllerBase();
+            var result =  controller.Successful() as OkNegotiatedContentResult<string>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("OK", result.Content);
+        }
+
+        [TestMethod]
+        public void Duplicate_ShouldReturnOkNegotiatedContentResultWithDuplicateMessage()
+        {
+            var controller = new ApiControllerBase();
+            var result = controller.Duplicate() as OkNegotiatedContentResult<string>;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Duplicate", result.Content);
+        }
+    }
+}
